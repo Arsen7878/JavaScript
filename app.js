2b6c6704f2cea099ba310052cc709909dfb12f55
Arsen Temurian <arsentim2611@gmail.com>
2021-08-05 20:59:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -761,49 +761,189 @@
 // console.log(bookShelf.updateBook('Haze', 'Dungeon chronicles'));
 
 // !Модуль 3 задача 41 - полный капец
-const atTheOldToad = {
-  potions: [
-    { name: 'Speed potion', price: 460 },
-    { name: 'Dragon breath', price: 780 },
-    { name: 'Stone skin', price: 520 },
-  ],
-  // Change code below this line
-  getPotions() {
-    return this.potions;
-  },
-  addPotion(potionName) {
-    for (const potion of this.potions) {
-      if (potion.name === potionName.name) {
-        return `Potion ${potionName.name} is already equipped!`;
-      }
-    }
-    this.potions.push(potionName);
-  },
-  removePotion(potionName) {
-    const { potions } = this;
+// const atTheOldToad = {
+//   potions: [
+//     { name: 'Speed potion', price: 460 },
+//     { name: 'Dragon breath', price: 780 },
+//     { name: 'Stone skin', price: 520 },
+//   ],
+//   // Change code below this line
+//   getPotions() {
+//     return this.potions;
+//   },
+//   addPotion(potionName) {
+//     for (const potion of this.potions) {
+//       if (potion.name === potionName.name) {
+//         return `Potion ${potionName.name} is already equipped!`;
+//       }
+//     }
+//     this.potions.push(potionName);
+//   },
+//   removePotion(potionName) {
+//     const { potions } = this;
 
-    for (let i = 0; i < potions.length; i += 1) {
-      const { name } = potions[i];
-      if (potionName === name) {
-        console.log(`Нашли ${potionName}, удаляю`);
-        potions.splice(i, 1);
-      }
-    }
-    return `Potion ${potionName} is not in inventory!`;
-  },
-  updatePotionName(oldName, newName) {
-    const { potions } = this;
-    for (const potion of potions) {
-      if (potion.name === oldName) {
-        potion.name = newName;
-      }
-    }
-  },
-  // Change code above this line
-};
+//     for (let i = 0; i < potions.length; i += 1) {
+//       const { name } = potions[i];
+//       if (potionName === name) {
+//         console.log(`Нашли ${potionName}, удаляю`);
+//         potions.splice(i, 1);
+//       }
+//     }
+//     return `Potion ${potionName} is not in inventory!`;
+//   },
+//   updatePotionName(oldName, newName) {
+//     const { potions } = this;
+//     for (const potion of potions) {
+//       if (potion.name === oldName) {
+//         potion.name = newName;
+//       }
+//     }
+//   },
+//   // Change code above this line
+// };
 
-console.log(atTheOldToad.getPotions());
+// console.log(atTheOldToad.getPotions());
 
-atTheOldToad.updatePotionName('Dragon breath', 'Polymorth');
+// atTheOldToad.updatePotionName('Dragon breath', 'Polymorth');
 
-console.table(atTheOldToad.potions);
+// console.table(atTheOldToad.potions);
+
+// ! Мщдуль 4 задача 3 КОЛБЭК-ФУНКЦИИ
+// function deliverPizza(pizzaName) {
+//   return `Доставляем пиццу ${pizzaName}.`;
+// }
+
+// function makePizza(pizzaName) {
+//   return `Пицца ${pizzaName} готовится, ожидайте...`;
+// }
+
+// // Пиши код ниже этой строки
+// function makeMessage(pizzaName, callback) {
+//   return callback(pizzaName);
+// }
+// console.log(makeMessage('Роял гранд', makePizza));
+
+// !Модуль 4 задача 6
+// const pizzaPalace = {
+//   pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
+//   order(pizzaName, onSuccess, onError) {
+//     const { pizzas } = this;
+//     for (const pizza of pizzas) {
+//       if (pizza === pizzaName) {
+//         return makePizza(pizzaName);
+//       }
+//     }
+//     return onOrderError(`В ассортименте нет пиццы с названием ${pizzaName}.`);
+//   },
+// };
+// // Пиши код выше этой строки
+
+// // Колбэк для onSuccess
+// function makePizza(pizzaName) {
+//   return `Ваш заказ принят. Готовим пиццу ${pizzaName}.`;
+// }
+
+// // Колбэк для onError
+// function onOrderError(error) {
+//   return `Ошибка! ${error}`;
+// }
+
+// // Вызовы метода с колбэками
+// console.log(pizzaPalace.order('Аль Копчино', makePizza, onOrderError));
+// pizzaPalace.order('Четыре нарезона', makePizza, onOrderError);
+// console.log(pizzaPalace.order('Биг майк', makePizza, onOrderError));
+// pizzaPalace.order('Венская', makePizza, onOrderError);
+
+// Модуль 4 задача №7
+// const orders = [
+//   { email: 'solomon@topmail.ua', dish: 'Burger' },
+//   { email: 'artemis@coldmail.net', dish: 'Pizza' },
+//   { email: 'jacob@mail.com', dish: 'Taco' },
+// ];
+
+// // Пиши код ниже этой строки
+// function composeMessage(position) {
+//   return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
+// }
+
+// const messages = [];
+// for (let i = 0; i < orders.length; i += 1) {
+//   const msg = composeMessage.call(orders[i], i + 1);
+//   console.log(composeMessage.call(orders[i], i + 1));
+//   messages.push(msg);
+// }
+
+// console.log(messages);
+
+// !Модуль 4 задача 9
+// const pizzaPalace = {
+//   company: 'Pizza Palace',
+// };
+
+// const burgerShack = {
+//   company: 'Burger Shack',
+// };
+
+// function composeMessage(customerName) {
+//   return `${customerName}, всегда рады вас видеть в «${this.company}».`;
+// }
+// // Пиши код ниже этой строки
+
+// const pizzaPalaceComposer = composeMessage.bind(pizzaPalace);
+// const pizzaPalaceMessage = pizzaPalaceComposer('Манго');
+
+// const burgerShackComposer = composeMessage.bind(burgerShack);
+// const burgerShackMessage = burgerShackComposer('Poly');
+// console.log(pizzaPalaceMessage);
+
+// !Модуль 4 задача 10
+// const service = {
+//   mailingList: ['mango@mail.com', 'poly@hotmail.de', 'ajax@jmail.net'],
+//   subscribe(email) {
+//     this.mailingList.push(email);
+//     return `Почта ${email} добавлена в рассылку.`;
+//   },
+//   unsubscribe(email) {
+//     this.mailingList = this.mailingList.filter((e) => e !== email);
+//     return `Почта ${email} удалена из рассылки.`;
+//   },
+// };
+
+// function logAndInvokeAction(email, action) {
+//   console.log(`Выполняем действие с ${email}.`);
+//   return action(email);
+// }
+
+// const firstInvoke = logAndInvokeAction(
+//   'kiwi@mail.uk',
+//   service.subscribe.bind(service)
+// );
+// console.log(firstInvoke);
+// // Почта kiwi@mail.uk добавлена в рассылку.
+
+// console.log(service.mailingList);
+// /* ['mango@mail.com',
+//     'poly@hotmail.de',
+//     'ajax@jmail.net',
+//     'kiwi@mail.uk']*/
+// const secondInvoke = logAndInvokeAction(
+//   'poly@hotmail.de',
+//   service.unsubscribe.bind(service)
+// );
+// console.log(secondInvoke);
+// // Почта poly@hotmail.de удалена из рассылки.
+
+// console.log(service.mailingList); // ['mango@mail.com', 'ajax@jmail.net', 'kiwi@mail.uk']
+
+// const customer = {
+//   firstName: 'Jacob',
+//   lastName: 'Mercer',
+//   getFullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+// };
+// function makeMessage(callback) {
+//   // callback() это вызов метода getFullName без объекта
+//   console.log(`Обрабатываем заявку от ${callback()}.`);
+// }
+// makeMessage(customer.getFullName.bind(customer));
